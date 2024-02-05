b67c33cd71709dee286437cc40cb81dcf12f24d0
Monica Nguyen-Hoang <nguyenhl@dickinson.edu>
2024-02-04 22:04:39 -0500

    Initial Commit

---
+++ MealPrep/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealPrep
+//
+//  Created by Linh on 2/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
